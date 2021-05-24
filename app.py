a9b67c6ae89b79aa8dda9d826e04658ffa6985ff
Deepanshu Sharma <73581139+Deepanshu291@users.noreply.github.com>
2021-05-24 12:54:27 +0530

    Add Admin Panel

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,redirect,url_for,render_template,request
-from datetime import datetime 
+from datetime import datetime
+from flask.signals import template_rendered 
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -27,23 +28,53 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
-@app.route('/index',methods=['GET','POST'])
+@app.route('/admin', methods=['GET', 'POST'])
 def home():
+   users = User.query.all()
+   return render_template('index.html', users=users)       
+
+@app.route('/add',methods=['GET','POST'])
+def Add():
     if request.method=='POST':
         user = User(  name=request.form['name'], message=request.form['message'] )
         db.session.add(user)
         db.session.commit()
+        return redirect('/admin')
     users = User.query.all()
-    return render_template('index.html', users=users)
+    return render_template('add.html', users=users)
 
-@app.route('/', methods=['GET', 'POST'])
-def Users():
-   if request.method=='POST':
-        name = request.form['name']
-        message = request.form['message']
-        if message == User.message:
-            redirect('/index')
-   return render_template('login.html')   
+ 
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    
+    if request.method=='POST':
+        name = request.form['title'],
+        message= request.form['msg']
+        users = User.query.filter_by(sno=sno).first()
+        users.name= name
+        users.message= message 
+        # users = User(  name=request.form['name'], message=request.form['message'] ).query.filter_by(sno=sno).first
+        db.session.add(users)
+        db.session.commit()
+        return redirect("/admin")
+       
+    users = User.query.filter_by(sno=sno).first()
+    return render_template('update.html', users=users)
+
+
+
+@app.route('/delete/<int:sno>', methods=['GET'])
+def delete(sno):
+   users = User.query.filter_by(sno=sno).first()
+   db.session.delete(users)
+   db.session.commit()
+   return redirect('/admin')
+
+@app.route('/delete', methods=['GET'])
+def deleteall():
+   db.session.query(User).delete()
+   db.session.commit()
+   return redirect('/admin')   
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
